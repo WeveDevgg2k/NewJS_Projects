c6142c9f98cd3c802145e2abb9163d0fb1cfff08
WevertonBrito <WevertonMB.Dev@gmail.com>
2023-07-03 12:53:32 -0300

    barra de progresso

---
+++ Barra_de_progresso/script.js
@@ -0,0 +1,23 @@
+const progressBar = document.querySelector(".progress")
+const previousBtn = document.querySelector("#prev-btn")
+const nextBtn = document.querySelector("#next-btn")
+
+let progress = 0
+
+function updateProgressBar() {
+  progressBar.style.width = progress + "%"
+}
+
+function nextStep() {
+  progress += 10
+  if (progress > 100) progress = 100
+  updateProgressBar()
+}
+function prevStep() {
+  progress -= 10
+  if (progress < 0) progress = 0
+  updateProgressBar()
+}
+
+nextBtn.addEventListener("click", nextStep)
+previousBtn.addEventListener("click", prevStep)
